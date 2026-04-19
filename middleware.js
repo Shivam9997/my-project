546faff597496136c8e1421579c3766482b08521
Shivam9997 <shivamrathore2805@gmail.com>
2026-04-20 01:09:02 +0530

    Fix formatting by adding new lines in middleware.js

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema , reviewSchema} = require("./schema.js");
 
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
@@ -10,6 +11,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     return res.redirect("/login");
   }
   next();
+
 };
 
 module.exports.saveRedirectUrl = (req, res, next) => {
